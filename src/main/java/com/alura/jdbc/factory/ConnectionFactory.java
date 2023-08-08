ee2ea6f479775121db9b915542d2c395ce7694a7
lionel staricoff <staricofflionel@hotmail.com>
2023-08-08 11:25:08 -0500

    creando clase producto

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,15 +1,29 @@
 package com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+	private DataSource dataSource;
+	
+	public ConnectionFactory() {
+		//variable de la libreria c3p0 para menejar conecciones
+		var PooledDataSource = new ComboPooledDataSource();
+		PooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		PooledDataSource.setUser("root");
+		PooledDataSource.setPassword("1234");
+		PooledDataSource.setMaxPoolSize(10);//cantidad de conexciones que se pueden abrir
+		
+		this.dataSource = PooledDataSource;
+	
+	}
 
 	public Connection recuperarConexion() throws SQLException {
-		return DriverManager.getConnection(
-                "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-                "root",
-                "1234");
+		return this.dataSource.getConnection();
+              
 	}
 }
---
+++ src/main/java/com/alura/jdbc/modelo/Producto.java
@@ -0,0 +1,13 @@
+package com.alura.jdbc.modelo;
+
+public class Producto {
+	
+	private Integer id;
+	
+	private String nombre;
+	
+	private String descripcion;
+	
+	private Integer cantidad;
+
+}
